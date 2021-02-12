ec55aabb706fe3690ac4512b71c6bf759902db63
kkkunny <51853085+kkkunny@users.noreply.github.com>
2021-02-12 11:05:30 +0800

    web更新

--- http/web/middleware.go
+++ http/web/middleware.go
@@ -7,7 +7,7 @@ func MidRequestLog(handler HandlerFunc) HandlerFunc {
 	return func(ctx *Context) {
 		ip := ctx.GetRequestIp()
 		if len(ip) > 0 {
-			fmt.Println(fmt.Sprintf("request: a new [%s] request from IP: %s", ctx.GetMethod(), ip))
+			fmt.Println(fmt.Sprintf("request: a new [%s] request from IP: %s to ROUTE: %s", ctx.GetMethod(), ip, ctx.GetUrl()))
 		}
 		handler(ctx)
 	}
